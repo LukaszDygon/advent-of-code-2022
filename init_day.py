74aaccea8cff3259f08659f842373c32939b376f
Lukasz Dygon <lukaszdygon@lukaszs-mbp.home>
2022-12-04 09:52:15 +0000

    run template for init script

--- init_day.py
+++ init_day.py
@@ -1,5 +1,6 @@
 import os
 import requests
+import shutil
 from  datetime import datetime
 
 AOC_SESSION = "53616c7465645f5f81977915b5c5753d5aa5e5b698bc03c3c778f83b5cf7437bfdaccaec69556d583ede178a92f39b19b01a081ce832e9a6cb74baab59cf5ede"
@@ -7,9 +8,13 @@ EMAIL = "godsofforgiveness@gmail.com"
 
 def init_day(day):
     aoc_url = f"https://adventofcode.com/2022/day/{day}/input"
-    os.mkdir(f"day{day}")
+    os.makedirs(f"day{day}", exist_ok=True)
+    
     with open(f"day{day}/input.txt", "w") as f:
         f.write(requests.get(aoc_url, cookies={"session": AOC_SESSION}, headers={"User-Agent": EMAIL}).text)
+
+    shutil.copyfile("run_template.py", f"day{day}/run.py")
+
 
 if __name__ == '__main__':
     day = datetime.now().day
---
+++ run_template.py
@@ -0,0 +1,11 @@
+from aoc_helpers import input_helper
+
+
+def main():
+    lines = input_helper.get_lines()
+
+    print("1:", "")
+    print("2:", "")
+
+if __name__ == "__main__":
+    main()
